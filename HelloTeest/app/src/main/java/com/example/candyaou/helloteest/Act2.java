79313d96b2789443ea0927a8c8e83a75f9a23c5e
Paspana Assarasee <candychupa_aou@hotmail.com>
2015-10-03 00:21:59 +0700

    CurrentLocation

--- HelloTeest/app/src/main/java/com/example/candyaou/helloteest/Act2.java
+++ HelloTeest/app/src/main/java/com/example/candyaou/helloteest/Act2.java
@@ -1,50 +1,96 @@
 package com.example.candyaou.helloteest;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by CandyAou on 10/2/15.
  */
-public class Act2 extends FragmentActivity implements OnMapReadyCallback {//implements View.OnClickListener{
+public class Act2 extends FragmentActivity implements GoogleMap.OnMyLocationChangeListener{//implements OnMapReadyCallback {//implements OnMapReadyCallback,,View.OnClickListener{
     //Button btn;
-
+    GoogleMap googleMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.act2);
 //        btn=(Button)findViewById(R.id.button2);
 //        btn.setOnClickListener(this);
 
         setContentView(R.layout.act2);
-        //setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        //setContentView(R.layout.activity_maps);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//
+//
+//        googleMap = mapFragment.getMap();
+//        googleMap.setMyLocationEnabled(true);
+//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//        Criteria criteria = new Criteria();
+//        String bestProvider = locationManager.getBestProvider(criteria, true);
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        if(status != ConnectionResult.SUCCESS) {
+            int request = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, request);
+            dialog.show();
+        }else{
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            googleMap = mapFragment.getMap();
+            googleMap.setMyLocationEnabled(true);
+            googleMap.setOnMyLocationChangeListener(this);
+
+        }
+
 
 
     }
 
+
+
+//    @Override
+//    public void onMapReady(GoogleMap map) {
+//        LatLng sydney = new LatLng(-34, 151);
+//        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//
+//    }
+
+
+
     @Override
-    public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-
+    public void onMyLocationChange(Location location) {
+        TextView tvLocation = (TextView) findViewById(R.id.textView1);
+        double lati = location.getLatitude();
+        double longi = location.getLongitude();
+        LatLng lat = new LatLng(lati,longi);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(lat));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        tvLocation.setText("Latitude : "+lati+",Longitude : "+longi);
 
     }
 
@@ -54,4 +100,7 @@ public class Act2 extends FragmentActivity implements OnMapReadyCallback {//impl
 //        startActivity(myintent);
 //
 //    }
+
+
+
 }
