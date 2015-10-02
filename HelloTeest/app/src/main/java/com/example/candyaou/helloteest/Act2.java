7d0110f535d0777f101adf778e12a774bfa3cdc5
Paspana Assarasee <candychupa_aou@hotmail.com>
2015-10-02 15:00:26 +0700

    Add map

--- HelloTeest/app/src/main/java/com/example/candyaou/helloteest/Act2.java
+++ HelloTeest/app/src/main/java/com/example/candyaou/helloteest/Act2.java
@@ -1,33 +1,57 @@
 package com.example.candyaou.helloteest;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.content.Intent;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by CandyAou on 10/2/15.
  */
-public class Act2 extends Activity implements View.OnClickListener{
-    Button btn;
+public class Act2 extends FragmentActivity implements OnMapReadyCallback {//implements View.OnClickListener{
+    //Button btn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.act2);
+//        btn=(Button)findViewById(R.id.button2);
+//        btn.setOnClickListener(this);
+
         setContentView(R.layout.act2);
-        btn=(Button)findViewById(R.id.button2);
-        btn.setOnClickListener(this);
+        //setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
     }
 
     @Override
-    public void onClick(View v) {
-        Intent myintent = new Intent(v.getContext(),MainActivity.class);
-        startActivity(myintent);
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
 
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        Intent myintent = new Intent(v.getContext(),MainActivity.class);
+//        startActivity(myintent);
+//
+//    }
 }
