dcd9e8ebeaf7425f32fb115301f84df60262ba1a
Paspana Assarasee <candychupa_aou@hotmail.com>
2015-11-14 14:41:27 +0700

    [WhereBus]Update user location
    Update user location to database

--- WhereBus/app/src/main/java/com/example/candyaou/wherebus/mainMap.java
+++ WhereBus/app/src/main/java/com/example/candyaou/wherebus/mainMap.java
@@ -5,8 +5,10 @@ package com.example.candyaou.wherebus;
  */
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
@@ -32,9 +34,21 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.w3c.dom.Document;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class mainMap extends FragmentActivity implements GoogleMap.OnMyLocationChangeListener{
     GoogleMap mMap;
@@ -43,10 +57,17 @@ public class mainMap extends FragmentActivity implements GoogleMap.OnMyLocationC
     LatLng fromPosition ;
     LatLng toPosition = new LatLng(13.70988, 100.50283);
     MarkerOptions startMark,endMark;
+    String username,password;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmap);
+
+        SharedPreferences sp = getSharedPreferences("wherebus", Context.MODE_PRIVATE);
+        username = sp.getString("username","NoUser");
+        password = sp.getString("password", "NoPass");
+
+
         int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
         if (status != ConnectionResult.SUCCESS) {
             int request = 10;
@@ -80,6 +101,13 @@ public class mainMap extends FragmentActivity implements GoogleMap.OnMyLocationC
 
         double lati = location.getLatitude();
         double longi = location.getLongitude();
+        try {
+            updatePlace(lati,longi);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
         fromPosition = new LatLng(lati, longi);
         mMap.clear();
         startMark = new MarkerOptions().position(fromPosition).title("Start");
@@ -117,6 +145,37 @@ public class mainMap extends FragmentActivity implements GoogleMap.OnMyLocationC
         }
         mMap.addPolyline(rectLine);
     }
+
+
+
+    public void updatePlace(double lat,double lon) throws URISyntaxException,HttpException {
+
+        String l = Double.toString(lat);
+        String a = Double.toString(lon);
+
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://172.20.10.4/user/update.php");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("username",username));
+            nameValuePairs.add(new BasicNameValuePair("lati",l));
+            nameValuePairs.add(new BasicNameValuePair("longi",a));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+    }
+
+
 
     @Override
     public void onBackPressed() {
